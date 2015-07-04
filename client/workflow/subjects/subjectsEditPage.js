c7eb5c51221d8faa9abe5e1d733c2994bd3ac5d8
Shekhar Prasad Rajak <shekharstudy@ymail.com>
2015-07-04 23:56:37 +0530

    buy section

--- client/workflow/subjects/subjectsEditPage.js
+++ client/workflow/subjects/subjectsEditPage.js
@@ -204,13 +204,6 @@ Template.subjectsEditPage.helpers({
 
 //-------upload image
 
-UploadsVehicle = new FS.Collection('uploadsVehicle', {
-  stores: [
-    //this will store files in ~/uploads folder on your filesystem
-    new FS.Store.FileSystem('uploadsVehicle', { path: '~/uploadsVehicle' })
-  ]
-});
-
 
 
   Template.uploadImageVehicle.helpers({
@@ -233,25 +226,3 @@ UploadsVehicle = new FS.Collection('uploadsVehicle', {
   });
 
 
-if (Meteor.isServer) {
-  UploadsVehicle.allow({
-    insert: function(userId, doc) {
-      //TODO validate if user can insert a file
-      return true;
-    },
-    update: function(userId, doc, fieldNames, modifier) {
-      //TODO validate if user can update a file
-      return true;
-    },
-    remove: function(userId, doc) {
-      //TODO validate if user can remove a file
-      return true;
-    },
-    //you would get access denied without this download allow option
-    //http://stackoverflow.com/questions/26136850/collectionfs-access-denied-403-error-meteor-js
-    download: function(userId, doc) {
-      //TODO validate if user can download a file
-      return true;
-    }
-  });
-}
--- shared/collections.js
+++ shared/collections.js
@@ -124,6 +124,35 @@ Uploads.allow({
 
 });
 
+UploadsVehicle = new FS.Collection("uploadsVehicle", {
+  stores: [
+    //this will store files in ~/uploads folder on your filesystem
+    new FS.Store.FileSystem("uploadsVehicle", { path: "~/uploadsVehicle" })
+  ]
+});
+
+UploadsVehicle.allow({
+    insert: function() {
+      //TODO validate if user can insert a file
+      alert('Submitted!');
+      return true;
+    },
+    update: function(userId, doc, fieldNames, modifier) {
+      //TODO validate if user can update a file
+      return true;
+    },
+    remove: function(userId, doc) {
+      //TODO validate if user can remove a file
+      return true;
+    },
+    //you would get access denied without this download allow option
+    //http://stackoverflow.com/questions/26136850/collectionfs-access-denied-403-error-meteor-js
+    download: function(userId, doc) {
+      //TODO validate if user can download a file
+      return true;
+    }
+
+});
 
 
 
